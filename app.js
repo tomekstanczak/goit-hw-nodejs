05642c0ac5debb75100faaa0b75101ac32035e1b
tomekstanczak <tomek.stanczak87@gmail.com>
2024-07-25 22:39:21 +0200

    clean up app file code

--- app.js
+++ app.js
@@ -1,37 +1,30 @@
 const express = require("express");
-const logger = require("morgan");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const jwtStrategy = require("./config/jwt.js");
 const path = require("path");
+const jwtStrategy = require("./config/jwt.js");
 
+const app = express();
 require("dotenv").config();
-jwtStrategy();
-const { DB_HOST: urlDb } = process.env;
 
-const connection = mongoose.connect(urlDb);
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.resolve(__dirname, "./public")));
+
+jwtStrategy();
 
 const contactsRouter = require("./routes/api/contacts");
 const authRouter = require("./routes/users/authUser.js");
 
-const app = express();
-
-const formatsLogger = app.get("env") === "development" ? "dev" : "short";
-
-app.use(logger(formatsLogger));
-app.use(cors());
-app.use(express.json());
-
+app.use("/api/contacts", contactsRouter);
+app.use("/api/users", authRouter);
 app.use("/api/contacts", contactsRouter);
 app.use("/api/users", authRouter);
 
-app.use(express.static(path.resolve(__dirname, "./public")));
-
-app.use("/api/contacts", contactsRouter);
-app.use("/api/users", authRouter);
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
 });
+
 app.use((err, req, res, next) => {
   if (err.name === "ValidationError") {
     return res.status(400).json({
@@ -43,6 +36,8 @@ app.use((err, req, res, next) => {
   });
 });
 
+const { DB_HOST: urlDb } = process.env;
+const connection = mongoose.connect(urlDb);
 connection
   .then(() => {
     console.log("Database connection successful");
